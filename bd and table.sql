c26cc24b53b3e55076d70eaf4f991321d7425954
lucaslessafilho <136939004+lucaslessafilho@users.noreply.github.com>
2023-06-19 20:12:13 -0300

    Update bd and table.sql

--- bd and table.sql
+++ bd and table.sql
@@ -1,1 +1,10 @@
+CREATE TABLE ouvidoria3 (
+   codigo int AUTO_INCREMENT,
+    titulo varchar (100),
+    descricao varchar (1000),
+    tipo varchar (100),
+    autor varchar (100),
+    primary key (codigo)
+);
+
 select* from ouvidoria3
